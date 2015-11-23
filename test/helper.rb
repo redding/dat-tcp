52589335fd966c4223a28570587c01491cbc8542
Collin Redding <TempestTTU@gmail.com>
2015-11-23 17:13:30 -0600

    Allow passing a logger to a server, remove debug option
    This updates the server to take a logger option. This replaces the
    debug option and will turn on logging for dat-tcp and its worker
    pool. This provides more flexibility for logging and doesn't force
    only logging to stdout.
    
    The server now only logs if an error occurs or when it closes its
    tcp server. This is because it now passes its logger to
    dat-worker-pool which handles logging that the worker pool is
    starting or shutting down. Both dat-tcp and dat-worker-pool logging
    is debug logging and in general shouldn't be enabled in a live
    environment.

--- test/helper.rb
+++ test/helper.rb
@@ -7,4 +7,13 @@ $LOAD_PATH.unshift(File.expand_path("../..", __FILE__))
 # require pry for debugging (`binding.pry`)
 require 'pry'
 
+require 'pathname'
+ROOT_PATH = Pathname.new(File.expand_path('../..', __FILE__))
+
+require 'logger'
+TEST_LOGGER = if ENV['DEBUG']
+  # don't show datetime in the logs
+  Logger.new(ROOT_PATH.join("log/test.log")).tap{ |l| l.datetime_format = '' }
+end
+
 require 'test/support/factory'
